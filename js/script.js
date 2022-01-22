c49764b9734ed056ea2c34fcc5a58ca61d686265
thcheetah777 <thcheetah777@gmail.com>
2022-01-22 11:07:47 -0800

    Create an array to store the current state of the game

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// The whole game
+let game = [
+  ["#", "#", "#"],
+  ["#", "#", "#"],
+  ["#", "#", "#"]
+];
+
 // Function to update the game
 function updateGame() {
   let game = document.getElementById("game");
