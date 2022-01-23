a36c360ad879893a31ca3bcc3bc4515460ea4012
thcheetah777 <thcheetah777@gmail.com>
2022-01-22 17:07:59 -0800

    Add to enemy class

--- js/Enemy.js
+++ js/Enemy.js
@@ -1,6 +1,17 @@
 // Enemy class
 class Enemy {
-  constructor(character, health, damage) {
+  constructor(character, health, damage, game) {
+    this.character = character;
+    this.health = health;
+    this.damage = damage;
+    this.game = game;
 
+    // Put in game
+    let random1 = Math.floor(Math.random() * game.length);
+    let random2 = Math.floor(Math.random() * game[random1]);
+    game[random1][random2] = this.character;
+  }
+  get game() {
+    return this.game;
   }
 }
